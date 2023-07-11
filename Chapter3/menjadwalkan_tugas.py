a67c0b6b62ca21251d1fbda79cfdaf4405d42431
ariyo21 <ariyoatmojo92@gmail.com>
2023-07-12 01:03:02 +0700

    UK_Ariyo_Studi_Kasus_PenjadwalanTiket

---
+++ Chapter3/menjadwalkan_tugas.py
@@ -0,0 +1,33 @@
+# Membuat list kosong untuk menyimpan jadwal dan daftar tugas
+jadwal_tugas = []
+daftar_tugas = []
+
+# Mengurutkan daftar tugas berdasarkan urutan alfabet
+daftar_tugas.sort()
+
+# Menginputkan jumlah tugas
+jumlah_tugas = int(input("Masukkan jumlah tugas: "))
+
+# Menginputkan nama tugas dan menyimpannya ke dalam list
+for i in range(jumlah_tugas):
+    tugas = input("Masukkan nama tugas ke-{}: ".format(i + 1))
+    daftar_tugas.append(tugas)
+
+# Menampilkan daftar tugas
+print("Daftar tugas:")
+for tugas in daftar_tugas:
+    print(tugas)
+
+# Menjadwalkan tugas secara rata-rata
+jumlah_tugas = len(daftar_tugas)
+jumlah_hari = int(input("Masukkan jumlah hari: "))
+tugas_per_hari = jumlah_tugas // jumlah_hari
+
+# Menyusun jadwal tugas
+for i in range(jumlah_hari):
+    jadwal = daftar_tugas[i*tugas_per_hari:(i+1)*tugas_per_hari]
+    jadwal_tugas.append(jadwal)
+
+# Menampilkan jadwal tugas
+for i, jadwal in enumerate(jadwal_tugas):
+    print("Hari {}: {}".format(i+1, jadwal))
--- Chapter3/myFunc.py
+++
@@ -1,5 +0,0 @@
-def myFunc(i):
-    print('Melakukan Pembayaran Tiket: %s' % i)
-    for j in range(1, i):
-        print('Pembayaran tiket telah selesai : nomor antrian %s' % j)
-    return
